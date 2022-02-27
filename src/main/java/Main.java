32c7356f3e42a2a87f13f119414a88b43fc8e5ff
Christoph <christoph.thonhauser@squer.at>
2022-02-27 18:31:20 +0100

    add additional console output

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,16 +4,23 @@ public class Main {
         var boyfriend = new Boyfriend();
 
         boyfriend.setCurrentMood(new Neutral());
+        System.out.println("NEUTRAL");
         boyfriend.giveKiss();
         boyfriend.drinkBeer();
         boyfriend.annoy();
+
+        System.out.println();
 
         boyfriend.setCurrentMood(new Annoyed());
+        System.out.println("ANNOYED");
         boyfriend.giveKiss();
         boyfriend.drinkBeer();
         boyfriend.annoy();
 
+        System.out.println();
+
         boyfriend.setCurrentMood(new Happy());
+        System.out.println("HAPPY");
         boyfriend.giveKiss();
         boyfriend.drinkBeer();
         boyfriend.annoy();
